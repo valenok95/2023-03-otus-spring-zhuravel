275d4de09a4810511f62bf47a1952a830108689b
valentin.zhuravel <valentin.zhuravel@x5.ru>
2023-04-13 09:49:16 +0300

    init

--- src/main/java/ru/otus/otusspringstudy/Main.java
+++ src/main/java/ru/otus/otusspringstudy/Main.java
@@ -1,18 +1,12 @@
 package ru.otus.otusspringstudy;
 
 import java.io.IOException;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import ru.otus.otusspringstudy.service.TestService;
 
 
 @ComponentScan
 public class Main {
     public static void main(String[] args) throws IOException {
-        AnnotationConfigApplicationContext ctx =
-                new AnnotationConfigApplicationContext(Main.class);
-        var testService = ctx.getBean(TestService.class);
-        testService.performTest();
     }
 
 }
--- src/main/java/ru/otus/otusspringstudy/exception/ReadResourceException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.otusspringstudy.exception;
-
-public class ReadResourceException extends RuntimeException {
-    public ReadResourceException(String message) {
-        super(message);
-    }
-}
